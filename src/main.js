6566862ea30e8d334956de94e12f48457af784ab
seajack <seajack@live.cn>
2018-09-13 00:02:13 +0800

    swiper

--- src/main.js
+++ src/main.js
@@ -2,15 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import 'styles/reset.css'
 import 'styles/border.css'
 import 'styles/iconfont.css'
 import fastClick from 'fastclick'
+import 'swiper/dist/css/swiper.css'
 
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
-
+Vue.use(VueAwesomeSwiper, )
 
 new Vue({
   router,
---
+++ src/views/Home/components/Swiper.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="wrapper">
+        <swiper :options="swiperOption">
+            <!-- slides -->
+            <swiper-slide v-for="item in swiperList" :key="item.id">
+            <img class="swiper-img" :src="item.imgUrl">
+            </swiper-slide>
+
+
+            <!-- Optional controls -->
+            <div class="swiper-pagination" slot="pagination"></div>
+        </swiper>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "HomeSwiper",
+        data() {
+            return {
+                swiperOption: {
+                    pagination: '.swiper-pagination',
+                    loop: true
+                },
+                swiperList: [{
+                    id: '001',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/bd/04554e7c67650302.jpg_750x200_4293d60a.jpg'
+                },{
+                    id: '002',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/f0/b39b64c33a62e002.jpg_750x200_3e961ce7.jpg'
+                },{
+                    id: '003',
+                    imgUrl: 'http://img1.qunarzz.com/piao/fusion/1809/6a/847ac7b98f10e202.jpg_750x200_f0593cb5.jpg'
+                }
+                ]
+            }
+        }
+    }
+</script>
+
+<style lang="stylus" scoped>
+    .wrapper >>> .swiper-pagination-bullet-active
+        background: #fff
+
+    .wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 31.25%
+        .swiper-img
+            width: 100%
+
+</style>
