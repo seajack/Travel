bc94568a18f47297348e6e191735a90458754d74
seajack <seajack@live.cn>
2018-09-14 15:36:35 +0800

    城市选择页面

--- src/views/City/City.vue
+++ src/views/City/City.vue
@@ -1,14 +1,19 @@
 <template>
-    <header-city></header-city>
+    <div>
+        <header-city></header-city>
+        <city-search></city-search>
+    </div>
 </template>
 
 <script>
 	import HeaderCity from './components/HeaderCity'
+	import CitySearch from './components/Search'
 
 	export default {
 		name: "City",
 		components: {
-			HeaderCity
+			HeaderCity,
+			CitySearch
 		}
 	}
 </script>
