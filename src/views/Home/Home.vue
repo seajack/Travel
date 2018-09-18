e73cf6ed489cc2547d0c2889c383acadc2a4529c
seajack <seajack@live.cn>
2018-09-18 18:01:31 +0800

    index-ajax失败

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,31 +1,44 @@
 <template>
-    <div class="home">
-        <home-header/>
-        <home-swiper></home-swiper>
-        <home-icons></home-icons>
-        <recommend></recommend>
-        <weekend></weekend>
-    </div>
+  <div class="home">
+    <home-header/>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <recommend></recommend>
+    <weekend></weekend>
+  </div>
 </template>
 
 <script>
-    // @ is an alias to /src
-    import HomeHeader from './components/Header'
-    import HomeSwiper from './components/Swiper'
-    import HomeIcons from './components/Icons'
-    import Recommend from './components/Recommend'
-    import Weekend from './components/Weekend'
-
-
-    export default {
-        name: 'home',
-        components: {
-            HomeHeader,
-            HomeSwiper,
-            HomeIcons,
-            Recommend,
-            Weekend
-
-        }
-    }
+	// @ is an alias to /src
+	import HomeHeader from './components/Header'
+	import HomeSwiper from './components/Swiper'
+	import HomeIcons from './components/Icons'
+	import Recommend from './components/Recommend'
+	import Weekend from './components/Weekend'
+	import axios from 'axios'
+	
+	export default {
+		name: 'home',
+		components: {
+			HomeHeader,
+			HomeSwiper,
+			HomeIcons,
+			Recommend,
+			Weekend
+		},
+		methods: {
+			getHomeInfo(){
+				axios.get('mock/index.json')
+            .then(this.getHomeInfoScc)
+      },
+      getHomeInfoScc(res){
+				console.log(res)
+				
+      }
+    },
+		mounted() {
+			this.getHomeInfo()
+			
+		}
+	}
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,41 @@
+let express = require('express');
+
+const app = express();
+
+let appData = '@/assets/city.json';
+let hotCities = appData.hotCities;
+
+let apiRoutes = express.Router();
+app.use('/api', apiRoutes);
+
 module.exports = {
-    configureWebpack: {
-        resolve: {
-            alias: {
-                'assets': '@/assets',
-                'styles': '@/assets/styles',
-                'views': '@/views',
-            }
-        }
-    },
+	configureWebpack: {
+		resolve: {
+			alias: {
+				'assets': '@/assets',
+				'styles': '@/assets/styles',
+				'views': '@/views',
+			}
+		}
+	},
+	devServer: {
+		// port: 1000,
+		// open: true,
+		// before(app) {
+		// 	app.get('/api/city', function (req, res) {
+		// 		res.json({code: 0, data: hotCities});
+		//
+		// 	});
+		// }
+		/*proxy:{
+		 	'/api':{
+		 		target: 'http://localhost:8080',
+		 		// changeOrigin: true,
+		 		// ws:true,
+		 		pathRewrite:{
+		 			'^/api': './mock'
+		 		}
+		 	}
+		 }*/
+	}
 }
